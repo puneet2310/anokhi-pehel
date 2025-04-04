7cac2ce9b0e77f9ebd43072daa41ab7a683e38d4
Puneet <puneetjnv289@gmail.com>
2025-04-04 22:33:19 +0530

    change the year in footer section

--- client/src/components/Dashboard/Footer.jsx
+++ client/src/components/Dashboard/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="mt-24">
       <p className="dark:text-gray-200 text-gray-700 text-center m-50">
-        © 2022 All rights reserved by anokhi-pehel.azurewebsites.net
+        © 2025 All rights reserved by anokhi-pehel.azurewebsites.net
       </p>
       <p className="dark:text-gray-200 text-gray-700 text-center m-5">
         The website is currently under maintenance.
